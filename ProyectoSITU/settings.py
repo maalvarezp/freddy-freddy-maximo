1a82b5b2f5e4275ebb85dc050570b0e536f91ca3
maximo <maalvarezp@unl.edu.ec>
2022-10-22 18:28:07 -0500

    .

--- ProyectoSITU/settings.py
+++ ProyectoSITU/settings.py
@@ -124,4 +124,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATIC_ROOT = BASE_DIR / "static/"
+#STATIC_ROOT = BASE_DIR / "static/"
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
