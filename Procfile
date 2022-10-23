8f6034197305b2534ce2ed0090a987cb9e21c45a
maximo <maalvarezp@unl.edu.ec>
2022-10-22 22:23:29 -0500

    .

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
-python manage.py collectstatic --noinput;
 web: gunicorn ProyectoSITU.wsgi
--- ProyectoSITU/settings.py
+++ ProyectoSITU/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -128,4 +129,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATIC_URL = "static/"
-MEDIAR_URL = BASE_DIR
+
+# Enable WhiteNoise's GZip compression of static assets.
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
